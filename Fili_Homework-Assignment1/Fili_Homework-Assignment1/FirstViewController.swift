ed0c7048892eb6b8ba8676780943e3296b7c2eae
Matt Fili <matthewjosephfili@gmail.com>
2016-09-19 16:41:01 -0500

    homework 1

---
+++ Fili_Homework-Assignment1/Fili_Homework-Assignment1/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Fili_Homework-Assignment1
+//
+//  Created by Matthew Fili on 9/13/16.
+//  Copyright © 2016 NSS. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
